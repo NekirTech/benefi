efab390a05f32446cd7b5727da1abc33ef178585
xety1337 <mr.frankfel@web.de>
2023-12-23 13:32:09 +0100

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,44 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <div class="header">
+      <div class="logo-container">
+        <img src="benefi_logo.png" alt="Café Logo" class="logo" />
+        <div class="text-h5 text-white logo-text">Benefi Café</div>
+      </div>
+    </div>
+
+    <q-page-container class="q-page">
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<style scoped>
+.header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background-color: #41658a; /* Hintergrundfarbe anpassen */
+  z-index: 1000;
+  padding: 10px;
+  box-shadow: 0px 2px 5px rgba(0, 0, 0, 0.1); /* Optional: Schatten für einen schwebenden Effekt */
+}
+
+.logo {
+  max-height: 80px; /* Passen Sie die Höhe nach Bedarf an */
+  max-width: 100%; /* Sicherstellen, dass das Logo responsiv bleibt */
+  margin: 0;
+}
+
+.q-page {
+  padding-top: 90px; /* Platz für den fixierten Header schaffen */
+}
+.logo-container {
+  display: flex;
+  align-items: center;
+}
+.logo-text {
+  margin-left: 10px; /* Abstand zwischen Logo und Text anpassen */
+}
+</style>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <q-carousel
+      v-model="selectedSlide"
+      transition-prev="slide-right"
+      transition-next="slide-left"
+      animated
+      arrows
+      control-color="primary"
+      class="rounded-borders bg-grey"
+      infinite
+      options="regular"
+      navigation
+      swipeable
+    >
+      <q-carousel-slide
+        v-for="(slide, index) in slides"
+        :key="index"
+        :name="slide.alt"
+      >
+        <q-img
+          class="rounded-borders full-height"
+          :src="slide.src"
+          fit="contain"
+        ></q-img>
+      </q-carousel-slide>
+    </q-carousel>
+
+    <div class="q-pa-md">
+      <h1 class="text-h5 q-mb-md">Welcome to Benefi Café</h1>
+      <p class="text-body1 q-mb-md">
+        Enjoy Coffee, Tea and homemade cakes in our lovely atmosphere!
+      </p>
+    </div>
+    <div class="q-pa-md">
+      <h1 class="text-h5 q-mb-md">Visit us every day 8:30 - 23</h1>
+      <p class="text-body1 q-mb-md">Directly opposite of the city square</p>
+    </div>
+    <div class="q-pa-md">
+      <h1 class="text-h6 q-mb-md">Imprint</h1>
+      <p class="text-body1">
+        Benefi Cafe<br />
+        Yeni, Atatürk Blv. No:138<br />
+        09270 Didim/Aydın<br />
+        <br />
+        E-Mail: info@benefi.cafe<br />
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+let selectedSlide = ref('Coffee');
+const slides = [
+  {
+    src: 'coffee.jpg',
+    alt: 'Coffee',
+  },
+  {
+    src: 'cake_1.jpg',
+    alt: 'Cake',
+  },
+  {
+    src: 'donut.jpg',
+    alt: 'Donut',
+  },
+];
+</script>
+
+<style scoped>
+/* Fügen Sie hier Ihre benutzerdefinierten Styles hinzu */
+</style>
