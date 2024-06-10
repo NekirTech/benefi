0ab622558a0405ed15e453cf6144668021ee3909
Felix <felix-nekir.frank@bundesbank.de>
2024-06-10 17:43:10 +0300

    menu page

---
+++ src/pages/MenuPage.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="content">
+    <h1 class="text-h4 q-mb-lg text-center">Menü</h1>
+    <q-list bordered padding>
+      <q-expansion-item
+        v-for="category in menu"
+        :key="category.name"
+        :label="category.name"
+        dense
+        header-class=" text-dark text-subtitle2 bg-accent
+"
+        expand-separator
+      >
+        <q-item
+          v-for="item in category.items"
+          :key="item.name"
+          class="q-px-md q-py-sm"
+        >
+          <q-item-section>
+            <q-item-label>{{ item.name }}</q-item-label>
+            <q-item-label caption v-if="item.description">{{
+              item.description
+            }}</q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <q-item-label v-if="item.smallPrice"
+              >{{ item.smallPrice }} / {{ item.largePrice }}
+            </q-item-label>
+
+            <q-item-label v-if="!item.smallPrice">
+              {{ item.largePrice }}
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </q-expansion-item>
+    </q-list>
+  </div>
+</template>
+
+<script setup>
+import menu from 'src/locales/menu.json';
+</script>
+
+<style scoped>
+.content2 {
+  margin-top: 80px; /* Platz für den fixierten Header schaffen */
+  padding: 20px;
+}
+</style>
